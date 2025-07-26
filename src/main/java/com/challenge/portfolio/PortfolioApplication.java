65ecdea9bdb70aefc858932156f14483014045e4
André <104792201+AndreRTN@users.noreply.github.com>
2025-07-26 14:10:54 -0300

    update project config

--- src/main/java/com/challenge/portfolio/PortfolioApplication.java
+++ src/main/java/com/challenge/portfolio/PortfolioApplication.java
@@ -2,12 +2,19 @@ package com.challenge.portfolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
-public class PortfolioApplication {
+@SpringBootApplication( scanBasePackages = { "com.challenge.portfolio" })
+public class PortfolioApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PortfolioApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PortfolioApplication.class);
 	}
 
 }
